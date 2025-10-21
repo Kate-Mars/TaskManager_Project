a956f101db71874396b13153cacdacc959ba3230
Kate-Mars <ekaterinamarisova@gmail.com>
2025-10-21 04:38:49 +0300

    Folders, Getters and Setters for Class Task

--- TaskManager/src/Task.java
+++ TaskManager/src/Task.java
@@ -1,2 +1,27 @@
+import jdk.jfr.Description;
+
 public class Task {
+    private String title;
+    private String description;
+    private String date;
+    private int priority;
+
+    public Task(String title, String description, String date, int priority){
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+    }
+
+    // Getters
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public String getDate() { return date; }
+    public int getPriority() { return priority; }
+
+    // Setters
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+    public void setDate(String date) { this.date = date; }
+    public void setPriority(int priority) { this.priority = priority; }
 }
