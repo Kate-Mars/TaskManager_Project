c771b2584697559c2e7e7ed93cd6dd357b15f3f5
Kate-Mars <ekaterinamarisova@gmail.com>
2025-10-21 04:28:24 +0300

    Added Main Menu state - Add task, Show tasks, Delete task, Edit task, Sort tasks, Search tasks, Exit

--- TaskManager/src/Main.java
+++ TaskManager/src/Main.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("    To-Do List    ");
+
+        while (true) {
+            System.out.println("1. Добавить задачу");
+            System.out.println("2. Показать задачи");
+            System.out.println("3. Удалить задачу");
+            System.out.println("4. Изменить задачу");
+            System.out.println("5. Сортировать задачи");
+            System.out.println("6. Поиск задач");
+            System.out.println("7. Выход");
+            String cmd = sc.nextLine();
+            switch (cmd) {
+                case "1": break;
+                case "2": break;
+                case "3": break;
+                case "4": break;
+                case "5": break;
+                case "6": break;
+                case "7": return;
+                default: System.out.println("Неверный ввод");
+            }
+        }
+    }
 }
